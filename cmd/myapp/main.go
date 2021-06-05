9afc0905d9ce0e92de13e019d8c47d175aaffeae
nanako0323 <08b39@gmail.com>
2021-06-05 15:27:10 +0900

    1-1

---
+++ cmd/myapp/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	basic "algorithmPractice/internal/basic/01"
+	"log"
+)
+
+func main() {
+	log.Println(basic.NotLoopPow(2, 2))
+}
---
+++ internal/basic/01/notloopPow.go
@@ -0,0 +1,7 @@
+package basic
+
+import "math"
+
+func NotLoopPow(x float64, n float64) float64 {
+	return math.Pow(x, n)
+}
